5d6f928c675817e608e85801ddc842a3a96615e4
ryananger <nightcrawling@gmail.com>
2023-02-06 12:44:59 -0500

    Initial commit.

---
+++ st.js
@@ -0,0 +1,12 @@
+var st = {
+  newState: function(name, state) {
+    const str = 'set' + name.charAt(0).toUpperCase() + name.slice(1);
+
+    const value = st[name] = state[0];
+    const set   = st[str]  = state[1];
+
+    return [value, set];
+  }
+};
+
+export default st;
